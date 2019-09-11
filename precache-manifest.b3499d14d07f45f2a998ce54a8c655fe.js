e7ab04529b7e6ef91ea0155286f1693ab0be7ddf
iwhonduras <iwhnpruebas@gmail.com>
2019-09-11 11:02:12 -0600

    Updates

---
+++ precache-manifest.b3499d14d07f45f2a998ce54a8c655fe.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1db3ef7a486beaed34a92d1e4e2ea16c",
+    "url": "/memory/index.html"
+  },
+  {
+    "revision": "339ce0bc4dcf4e66e4a0",
+    "url": "/memory/static/css/main.712f3d8d.chunk.css"
+  },
+  {
+    "revision": "47d4abf8b5c0fc2c3922",
+    "url": "/memory/static/js/2.fbd2d191.chunk.js"
+  },
+  {
+    "revision": "339ce0bc4dcf4e66e4a0",
+    "url": "/memory/static/js/main.06724287.chunk.js"
+  },
+  {
+    "revision": "94f647c09a573e59acd4",
+    "url": "/memory/static/js/runtime~main.91f5a326.js"
+  },
+  {
+    "revision": "db5781c5e706a7cbc9cfb5a842b9d660",
+    "url": "/memory/static/media/Celebremos.db5781c5.png"
+  },
+  {
+    "revision": "c2b2b83ddebe541acf9bd596727f16ed",
+    "url": "/memory/static/media/Fondoazul.c2b2b83d.png"
+  },
+  {
+    "revision": "e0ae5655f65013b9cdf64248e000eb85",
+    "url": "/memory/static/media/Jugar.e0ae5655.png"
+  },
+  {
+    "revision": "9487c69eb5dab82cbfbffa3ebeed0a44",
+    "url": "/memory/static/media/LogoBlanco.9487c69e.png"
+  },
+  {
+    "revision": "b33cfc26e4ee4481d6812314d48f3ca9",
+    "url": "/memory/static/media/VerMas.b33cfc26.png"
+  },
+  {
+    "revision": "ba8d1abe7090c55852e87d0e012ff51b",
+    "url": "/memory/static/media/background2.ba8d1abe.jpg"
+  },
+  {
+    "revision": "d19461347381adf704acd549985338ea",
+    "url": "/memory/static/media/celebremos2.d1946134.png"
+  },
+  {
+    "revision": "27b5e57665f234cf62bb06a2d5b8e65b",
+    "url": "/memory/static/media/centroamerica.27b5e576.png"
+  },
+  {
+    "revision": "6a2c7680a4a4f8dab6d1050417870071",
+    "url": "/memory/static/media/guatemala.6a2c7680.png"
+  },
+  {
+    "revision": "e696f212fecb8269b62e75a53f7c3588",
+    "url": "/memory/static/media/honduras2.e696f212.png"
+  },
+  {
+    "revision": "11d6c53150628da9374cc4af73cdd019",
+    "url": "/memory/static/media/nicaragua.11d6c531.png"
+  },
+  {
+    "revision": "440a8ddc4429be429d8f807b44351c28",
+    "url": "/memory/static/media/ninos.440a8ddc.jpg"
+  },
+  {
+    "revision": "9272fdaf754dec9e7194fdfaad2ad46d",
+    "url": "/memory/static/media/salvador.9272fdaf.png"
+  },
+  {
+    "revision": "e61d6085ff1f4705788f3d940501043f",
+    "url": "/memory/static/media/terra.e61d6085.png"
+  },
+  {
+    "revision": "60deafd2327b4f0187d7488fc7113cef",
+    "url": "/memory/static/media/terraLogo.60deafd2.png"
+  }
+]);
