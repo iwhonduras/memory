99e87ba235a35078112754c64c30260956b98ff5
Walther Carrasco <waemcame@gmail.com>
2019-09-11 14:28:05 -0600

    Changed celebremos image

--- src/containers/menu/index.js
+++ src/containers/menu/index.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 
 import FondoAzul from '../../Assets/mic/Fondoazul.png';
 import ninos from '../../Assets/mic/ninos.jpg';
-import celebremos3 from '../../Assets/mic/Celebremos3.png';
+import celebremos4 from '../../Assets/mic/Celebremos4.png';
 import jugar from '../../Assets/mic/Jugar.png';
 import ver from '../../Assets/mic/VerMas.png';
 import './index.css';
@@ -16,7 +16,7 @@ export default class Menu extends Component {
           <img src={ninos} className="ninos" alt="" />
         </div>
         <div className="top">
-          <img src={celebremos3} className="celebremos" alt="" />
+          <img src={celebremos4} className="celebremos" alt="" />
           <img
             src={jugar}
             className="celebremos boton"
